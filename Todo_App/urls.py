825b91f8b9f98b40ceeaa78ebf1140435aba1683
cherucole <cheruyotcheruyot@gmail.com>
2019-03-12 12:16:32 +0300

    fetching individual item

--- Todo_App/urls.py
+++ Todo_App/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$', views.homepage, name='homepage')
+    url('^$', views.homepage, name='homepage'),
+    url(r'^details/item/(?P<id>\d+)', views.item, name='item')
+    # url(r'^details/item/(\d+)', views.item, name='item')
+
+
 ]
--- Todo_App/views.py
+++ Todo_App/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 def homepage(request):
-    return render(request, 'index.html')
+    items = Item.objects.all()
+    number = len(items)
+    context = {
+        'list_items': items,
+        'name': 'cherucole',
+        'length': number  # if something is referenced above do not put it in quotes because it already has a value so just use name
+    }
+    # items = Item.objects.all()
+    return render(request, 'index.html', context)
+
+
+def item(request, id):
+    item = Item.objects.get(id=id)
+    context = {
+        'list_item': item,
+        'name': 'cherucole'
+    }
+    return render(request, 'details.html', context)
