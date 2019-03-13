8cd2f4e973ed4ec981a510b96a44548955c5cbd2
cherucole <cheruyotcheruyot@gmail.com>
2019-03-13 11:47:38 +0300

    now to work on error handling

--- Todo_App/urls.py
+++ Todo_App/urls.py
@@ -6,7 +6,6 @@ app_name = 'items'
 urlpatterns = [
     url('^$', views.homepage, name='homepage'),
     url(r'^item/(?P<id>\d+)', views.item, name='item'),
-    url(r'^item/(?P<id>\d+)/delete', views.item_delete, name='item_delete'),
     url(r'^delete/(?P<id>[0-9]+)/$', views.todo_delete, name='todo_delete'),
 
     url(r'^add', views.add_item, name='add_item')
--- Todo_App/views.py
+++ Todo_App/views.py
@@ -6,17 +6,18 @@ from .models import *
 def homepage(request):
     items = Item.objects.all()
     number = len(items)
+
     context = {
         'list_items': items,
         'name': 'cherucole',
         'length': number  # if something is referenced above do not put it in quotes because it already has a value so just use name
     }
-    # items = Item.objects.all()
     return render(request, 'index.html', context)
 
 
 def item(request, id):
     item = Item.objects.get(id=id)
+
     context = {
         'list_item': item,
         'name': 'cherucole'
@@ -24,25 +25,9 @@ def item(request, id):
     return render(request, 'details.html', context)
 
 
-def item_delete(request, id):
-    item = get_object_or_404(Item, id=id)
-    if request.method == 'POST':
-        item.delete()
-        return redirect('/')
-    return render(request, 'index.html', {'list_item': item})
-
-
-def todo_delete(request, id):
-    item = get_object_or_404(Item, id=id)  # Get your current item
-    if request.method == 'POST':         # If method is POST,
-        item.delete()                     # delete the item.
-        return redirect('/')             # Finally, redirect to the homepage.
-    return render(request, 'details.html', {'list_item': item})
-    # If method is not POST, render the default template.
-
-
 def add_item(request):
     if(request.method == 'POST'):
+
         title = request.POST['title']
         body = request.POST['body']
 
@@ -53,3 +38,15 @@ def add_item(request):
 
     else:
         return render(request, 'add.html')
+
+
+def todo_delete(request, id):
+    item = get_object_or_404(Item, id=id)  # Get your current item
+
+    if request.method == 'POST':         # If method is POST,
+
+        item.delete()                     # delete the item.
+        return redirect('/')             # Finally, redirect to the homepage.
+
+    return render(request, 'details.html', {'list_item': item})
+    # If method is not POST, render the default template.
